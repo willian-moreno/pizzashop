a38eafe0aa3312a7e572d2d825a34354ea3951cc
willianam <willianam@uninove.br>
2024-05-20 17:34:37 -0300

    feat:  structuring sign-in content

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,11 +1,28 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 import { Helmet } from 'react-helmet-async'
 
 export function SignIn() {
   return (
     <>
       <Helmet title="Login" />
-      <main>
-        <h1>SignIn</h1>
+      <main className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tighter">Acessar painel</h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro!
+            </p>
+          </div>
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" />
+            </div>
+            <Button type="submit">Acessar painel</Button>
+          </form>
+        </div>
       </main>
     </>
   )
