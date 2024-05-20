9288847e20ce6c41707901faf377af9aa6f990a8
willianam <willianam@uninove.br>
2024-05-20 14:36:19 -0300

    chore: starting and configuring the project

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import react from '@vitejs/plugin-react'
+import path from 'node:path'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/',
+  plugins: [
+    react({
+      include: '**/*.tsx',
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
+})
