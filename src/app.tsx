74b72cfce747fe9f6f36ce1580e7913bc856a145
willianam <willianam@uninove.br>
2024-05-21 12:07:53 -0300

    feat: implementing dark theme

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,18 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
+import { ThemeProvider } from './components/theme/theme-provider'
 import './global.css'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <RouterProvider router={router} />
-      <Toaster richColors />
+      <ThemeProvider storageKey="@pizzashop:theme-1.0.0" defaultTheme="dark">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <RouterProvider router={router} />
+        <Toaster richColors />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
