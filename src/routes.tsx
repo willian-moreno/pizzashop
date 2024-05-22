dd0e54fb533a71e42a02ebc3707c9bc366b276c4
willianam <willianam@uninove.br>
2024-05-22 11:00:24 -0300

    feat: adding not found page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom'
+import { NotFound } from './pages/404'
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
@@ -10,6 +11,7 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       { path: '/', element: <Dashboard /> },
       { path: '/orders', element: <Orders /> },
@@ -18,6 +20,7 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AuthLayout />,
+    errorElement: <NotFound />,
     children: [
       { path: '/sign-in', element: <SignIn /> },
       { path: '/sign-up', element: <SignUp /> },
