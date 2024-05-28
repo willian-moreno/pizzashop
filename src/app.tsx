4505883a614b471374be1b6ebf906c7fb177bf61
willianam <willianam@uninove.br>
2024-05-28 10:53:26 -0300

    chore: adding react query to manage requests

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,10 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 import { ThemeProvider } from './components/theme/theme-provider'
 import './global.css'
+import { queryClient } from './lib/react-query'
 import { router } from './routes'
 
 export function App() {
@@ -10,7 +12,9 @@ export function App() {
     <HelmetProvider>
       <ThemeProvider storageKey="@pizzashop:theme-1.0.0" defaultTheme="dark">
         <Helmet titleTemplate="%s | pizza.shop" />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
         <Toaster richColors />
       </ThemeProvider>
     </HelmetProvider>
