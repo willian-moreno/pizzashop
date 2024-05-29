f2aacf931f46b1288b31e88cf993420355fb0c6d
willianam <willianam@uninove.br>
2024-05-29 10:58:23 -0300

    feat: updating account menu data
    creating managed restaurant and profile routes to include in account

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,3 +1,6 @@
+import { getManagedRestaurant } from '@/api/get-managed-restaurant'
+import { getProfile } from '@/api/get-profile'
+import { useQuery } from '@tanstack/react-query'
 import { Building, ChevronDown, LogOut } from 'lucide-react'
 import { Button } from './ui/button'
 import {
@@ -10,21 +13,29 @@ import {
 } from './ui/dropdown-menu'
 
 export function AccountMenu() {
+  const { data: profile } = useQuery({
+    queryKey: ['profile'],
+    queryFn: getProfile,
+  })
+
+  const { data: managedRestaurant } = useQuery({
+    queryKey: ['managed-restaurant'],
+    queryFn: getManagedRestaurant,
+  })
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" className="flex select-none items-center gap-2">
-          Pizza Shop
+          {managedRestaurant?.name}
           <ChevronDown className="h-4 w-4" />
         </Button>
       </DropdownMenuTrigger>
 
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col">
-          <span>Willian Moreno</span>
-          <span className="text-sm font-normal text-muted-foreground">
-            willian_moreno@outlook.com
-          </span>
+          <span>{profile?.name}</span>
+          <span className="text-sm font-normal text-muted-foreground">{profile?.email}</span>
         </DropdownMenuLabel>
 
         <DropdownMenuSeparator />
