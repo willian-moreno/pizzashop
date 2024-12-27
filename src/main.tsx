e74b3c6012ef9dba00e3c0b45eaf8049bbafec8e
Willian Moreno <willian_moreno@outlook.com>
2024-12-27 11:57:11 -0300

    feat: Criando setup Mock Service Worker (MSW)

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { enableMSW } from './api/mocks'
 import { App } from './app'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+enableMSW().then(async () => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+})
