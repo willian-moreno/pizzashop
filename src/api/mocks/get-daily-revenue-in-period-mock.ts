535617f63fa03f86d39a26740d92c8544e3e4122
Willian Moreno <willian_moreno@outlook.com>
2024-12-30 15:51:29 -0300

    refactor: Filtrando mock de receita no período por datas

--- src/api/mocks/get-daily-revenue-in-period-mock.ts
+++ src/api/mocks/get-daily-revenue-in-period-mock.ts
@@ -1,36 +1,57 @@
 import { http, HttpResponse } from 'msw'
 import { GetDailyRevenueInPeriodResponse } from '../get-daily-revenue-in-period'
 
+const oneDay = 1000 * 60 * 60 * 24
+
+const dailyRevenueInPeriod = [
+  {
+    date: new Date().toLocaleDateString(),
+    receipt: 1568,
+  },
+  {
+    date: new Date(Date.now() + oneDay).toLocaleDateString(),
+    receipt: 2005,
+  },
+  {
+    date: new Date(Date.now() + oneDay * 2).toLocaleDateString(),
+    receipt: 1798,
+  },
+  {
+    date: new Date(Date.now() + oneDay * 3).toLocaleDateString(),
+    receipt: 1856,
+  },
+  {
+    date: new Date(Date.now() + oneDay * 4).toLocaleDateString(),
+    receipt: 2564,
+  },
+  {
+    date: new Date(Date.now() + oneDay * 5).toLocaleDateString(),
+    receipt: 1965,
+  },
+]
+
 export const getDailyRevenueInPeriodMock = http.get<never, never, GetDailyRevenueInPeriodResponse>(
   '/metrics/daily-receipt-in-period',
-  async () => {
-    const oneDay = 1000 * 60 * 60 * 24
+  async ({ request }) => {
+    const { searchParams } = new URL(request.url)
 
-    return HttpResponse.json([
-      {
-        date: new Date().toLocaleDateString(),
-        receipt: 1568,
-      },
-      {
-        date: new Date(Date.now() + oneDay).toLocaleDateString(),
-        receipt: 2005,
-      },
-      {
-        date: new Date(Date.now() + oneDay * 2).toLocaleDateString(),
-        receipt: 1798,
-      },
-      {
-        date: new Date(Date.now() + oneDay * 3).toLocaleDateString(),
-        receipt: 1856,
-      },
-      {
-        date: new Date(Date.now() + oneDay * 4).toLocaleDateString(),
-        receipt: 2564,
-      },
-      {
-        date: new Date(Date.now() + oneDay * 5).toLocaleDateString(),
-        receipt: 1965,
-      },
-    ])
+    const from = searchParams.get('from')
+    const to = searchParams.get('to') ?? from
+
+    let filteredDailyRevenueInPeriod = dailyRevenueInPeriod
+
+    if (from && to) {
+      const fromDate = new Date(from)
+      const toDate = new Date(to)
+
+      filteredDailyRevenueInPeriod = filteredDailyRevenueInPeriod.filter((item) => {
+        const [day, month, year] = item.date.split('/')
+        const currentDate = new Date(+year, +month - 1, +day)
+
+        return currentDate >= fromDate && currentDate <= toDate
+      })
+    }
+
+    return HttpResponse.json(filteredDailyRevenueInPeriod)
   },
 )
