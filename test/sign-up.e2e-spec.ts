e175d29dfb2a4c2ef0c676e4125ada562bb20fbf
Willian Moreno <willian_moreno@outlook.com>
2024-12-30 19:09:21 -0300

    chore: Adicionando espaços linhas testes e2e

--- test/sign-up.e2e-spec.ts
+++ test/sign-up.e2e-spec.ts
@@ -2,10 +2,12 @@ import { expect, test } from '@playwright/test'
 
 test('sign up successfully', async ({ page }) => {
   await page.goto('/sign-up', { waitUntil: 'networkidle' })
+
   await page.locator('#restaurantName').fill('Pizza Shop')
   await page.locator('#managerName').fill('John Doe')
   await page.locator('#email').fill('johndoe@example.com')
   await page.locator('#phone').fill('11987456321')
+
   await page.getByRole('button', { name: 'Finalizar cadastro' }).click()
 
   const toast = page.getByText('Resturante cadastrado com sucesso.')
@@ -15,6 +17,7 @@ test('sign up successfully', async ({ page }) => {
 
 test('sign up with some field empty', async ({ page }) => {
   await page.goto('/sign-up', { waitUntil: 'networkidle' })
+
   await page.getByRole('button', { name: 'Finalizar cadastro' }).click()
 
   const toast = page.getByText('Erro ao cadastrar restaurante.')
@@ -24,6 +27,7 @@ test('sign up with some field empty', async ({ page }) => {
 
 test('navigate to new login page', async ({ page }) => {
   await page.goto('/sign-up', { waitUntil: 'networkidle' })
+
   await page.getByRole('link', { name: 'Fazer login' }).click()
 
   expect(page.url()).toContain('/sign-in')
