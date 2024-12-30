709beb76c2e4a5c6309dd31cd2917c5ce874bfd2
Willian Moreno <willian_moreno@outlook.com>
2024-12-30 18:54:00 -0300

    test: Criando teste E2E Sign Up

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -46,7 +46,7 @@ export function SignUp() {
         },
       })
     } catch (error) {
-      toast.error('Error ao cadastrar restaurante.')
+      toast.error('Erro ao cadastrar restaurante.')
     }
   }
 
---
+++ test/sign-up.e2e-spec.ts
@@ -0,0 +1,30 @@
+import { expect, test } from '@playwright/test'
+
+test('sign up successfully', async ({ page }) => {
+  await page.goto('/sign-up', { waitUntil: 'networkidle' })
+  await page.locator('#restaurantName').fill('Pizza Shop')
+  await page.locator('#managerName').fill('John Doe')
+  await page.locator('#email').fill('johndoe@example.com')
+  await page.locator('#phone').fill('11987456321')
+  await page.getByRole('button', { name: 'Finalizar cadastro' }).click()
+
+  const toast = page.getByText('Resturante cadastrado com sucesso.')
+
+  expect(toast).toBeVisible()
+})
+
+test('sign up with some field empty', async ({ page }) => {
+  await page.goto('/sign-up', { waitUntil: 'networkidle' })
+  await page.getByRole('button', { name: 'Finalizar cadastro' }).click()
+
+  const toast = page.getByText('Erro ao cadastrar restaurante.')
+
+  expect(toast).toBeVisible()
+})
+
+test('navigate to new login page', async ({ page }) => {
+  await page.goto('/sign-up', { waitUntil: 'networkidle' })
+  await page.getByRole('link', { name: 'Fazer login' }).click()
+
+  expect(page.url()).toContain('/sign-in')
+})
